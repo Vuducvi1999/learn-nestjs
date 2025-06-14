ba99fc63cebcc41273fdcaf9280e44c972f3b08e
Vuducvi1999 <vuducvi20@gmail.com>
2025-06-14 09:51:24 +0700

    redo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeeModule } from './coffee/coffee.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    CoffeeModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        return {
-          type: 'postgres',
-          host: configService.get<string>('DB_HOST'),
-          port: configService.get<number>('DB_PORT'),
-          username: configService.get<string>('DB_USERNAME'),
-          password: configService.get<string>('DB_PASSWORD'),
-          database: configService.get<string>('DB_NAME'),
-          synchronize: false,
-          entities: ['./entities/*.entity.ts'],
-        };
-      },
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
