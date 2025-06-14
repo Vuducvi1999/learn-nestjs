c520ccc70737ccee87ce08c6b9bb3664e81d7bd8
Vuducvi1999 <vuducvi20@gmail.com>
2025-06-14 09:51:24 +0700

    redo

--- src/coffee/coffee.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CoffeeService } from './coffee.service';
-import { CoffeeController } from './coffee.controller';
-
-@Module({
-  providers: [CoffeeService],
-  controllers: [CoffeeController],
-})
-export class CoffeeModule {}
