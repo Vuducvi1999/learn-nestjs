70d91381e26c61d41fb3a61e365fc009acaafdc0
Vuducvi1999 <vuducvi20@gmail.com>
2025-07-02 10:08:19 +0700

    back to nest new

--- src/coffee/dto/create-coffee.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsArray, IsString } from 'class-validator';
-
-export class CreateCoffeeDto {
-  @IsString()
-  name: string;
-
-  @IsArray()
-  flavors: string[];
-}
--- src/configs/database.config.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export const databaseConfig = registerAs('dbConfig', () => ({
-  host: process.env.DB_HOST || 'localhost',
-  port: +(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-}));
