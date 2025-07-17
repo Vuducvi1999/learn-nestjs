080b552b39d3ae1d3ae1457ac0f63bf6948126df
Vuducvi1999 <vuducvi20@gmail.com>
2025-07-17 22:17:04 +0700

    setup env and mongoose connection

---
+++ env-validation.ts
@@ -0,0 +1,25 @@
+import z from 'zod';
+
+const envSchema = z.object({
+  MONGO_URI: z.string(),
+  MONGO_USER: z.string(),
+  MONGO_PASSWORD: z.string(),
+  MONGO_DB: z.string(),
+  MONGO_HOST: z.string(),
+  MONGO_PORT: z.coerce.number(),
+});
+
+export const envValidation = (config: Record<string, unknown>) => {
+  const parsedConfig = envSchema.safeParse(config);
+
+  console.log(parsedConfig);
+
+  if (!parsedConfig.success) {
+    console.error('Invalid environment variables:', parsedConfig.error.message);
+    process.exit(1); // Exit the application if validation fails
+  }
+
+  return parsedConfig.data;
+};
+
+export type EnvType = z.infer<typeof envSchema>;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EnvType, envValidation } from 'env-validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      validate: envValidation,
+      envFilePath:
+        process.env.NODE_ENV !== 'production'
+          ? `.env.${process.env.NODE_ENV}`
+          : '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<EnvType>) => {
+        return {
+          uri: configService.get('MONGO_URI', { infer: true }),
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
